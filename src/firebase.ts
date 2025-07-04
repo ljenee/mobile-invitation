9fd72d7dc757f7e05da4217855ebd5b340f38e11
임지 <j13un@imjiui-MacBookPro.local>
2025-07-04 13:03:46 +0900

    update info for my mobile invitation

--- src/firebase.ts
+++ src/firebase.ts
@@ -4,7 +4,14 @@ import { getDatabase } from 'firebase/database';
 // TODO: https://firebase.google.com/docs/web/setup?hl=ko
 // 가이드에 따라 firebase config를 설정합니다.
 const firebaseConfig = {
-
+    apiKey: "AIzaSyAs4m6pOBGfwj7sx6x63F2AsvaqFEFCNfI",
+    authDomain: "mobile-invitation-7ceb9.firebaseapp.com",
+    databaseURL: "https://mobile-invitation-7ceb9-default-rtdb.firebaseio.com",
+    projectId: "mobile-invitation-7ceb9",
+    storageBucket: "mobile-invitation-7ceb9.firebasestorage.app",
+    messagingSenderId: "40522096369",
+    appId: "1:40522096369:web:1d2b3690a2c0179cabd350",
+    measurementId: "G-L6FC3SQG8Q"
 };
 
 export const firebaseApp = initializeApp(firebaseConfig);
